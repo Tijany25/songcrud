26577c98cc0e2e581f1fa5647d6049559cd0efc8
Tijany25 <92982320+Tijany25@users.noreply.github.com>
2022-10-27 22:50:03 +0100

    models

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,20 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=40)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.CharField(max_length=40)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=4000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
